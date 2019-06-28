d7b7adcc8b0c3fb2f357038a5543470e4a2f8579
daisuke <dsaito85a@gmail.com>
2019-06-28 15:44:07 +0900

    文言修正

--- rune/resources/views/welcome.blade.php
+++ rune/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="container">
             <h2>お小遣いアプリ ルーン</h2>
-            <p>5歳からできるシールで作るお小遣い帳</p>
+            <p>ママが子供のお小遣い帳を賢く管理するアプリ</p>
             <h2>特徴</h2>
             <p>お小遣いのレートを決められる</p>
             <p>お手伝いの内容を管理できる</p>
