11f510fa3c51f7e33c7dad013bfff8414afa4506
daisuke <dsaito85a@gmail.com>
2019-06-28 15:47:40 +0900

    リンク修正

--- rune/resources/views/help/create.blade.php
+++ rune/resources/views/help/create.blade.php
@@ -4,9 +4,9 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="container">
-            <button type="button" class="btn btn-primary btn-lg btn-block my-4" onclick="location.href='/'">お皿洗い(20ルーン)</button>
-            <button type="button" class="btn btn-primary btn-lg btn-block my-4" onclick="location.href='/'">お風呂掃除(40ルーン)</button>
-            <button type="button" class="btn btn-primary btn-lg btn-block my-4" onclick="location.href='/'">おつかい(60ルーン)</button>
+            <button type="button" class="btn btn-primary btn-lg btn-block my-4" onclick="location.href='/home'">お皿洗い(20ルーン)</button>
+            <button type="button" class="btn btn-primary btn-lg btn-block my-4" onclick="location.href='/home'">お風呂掃除(40ルーン)</button>
+            <button type="button" class="btn btn-primary btn-lg btn-block my-4" onclick="location.href='/home'">おつかい(60ルーン)</button>
         </div>
     </div>
 </div>
